327b60b7b080bc5b25a536c9d0ea5c1ea51c6f11
3dgyGamer <jakepark2908@gmail.com>
2023-09-14 23:01:36 -0400

    stuff

---
+++ windo/windows.py
@@ -0,0 +1,20 @@
+import os
+
+# to use powershell, please do subprocess.call('C:\Windows\System32\powershell.exe CMD-let', shell=True) or run where powershell in the windows mahcine and change location accordingly
+
+### setup 
+def checkIfRoot():
+    pass
+
+
+####cleaning up
+
+
+####security policies 
+
+def menu():
+    pass
+#main function for executable
+if __name__ == "__main__":
+    menu()
+    print("hello")
